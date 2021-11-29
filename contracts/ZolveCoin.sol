a3b8492ed0e61d1556c5f352688af141ceac947d
dobiedad <leomdiv@gmail.com>
2021-11-29 21:33:09 +0000

    test transfers

--- contracts/ZolveCoin.sol
+++ contracts/ZolveCoin.sol
@@ -2,18 +2,34 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract ZolveCoin {
-  uint public totalSupply = 210000000;
+  uint public totalSupply = 2100000000000000;
   uint public decimals = 8;
   string public name = "Zolve Coin";
   string public symbol = "ZLV";
 
   mapping (address => uint) balances;
 
+  event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
 	constructor() {
 		balances[tx.origin] = 1;
 	}
 
-  function getBalanceOfAddress(address addr) public view returns(uint) {
+  function balanceOf(address addr) public view returns(uint) {
 		return balances[addr];
 	}
+
+  function transfer(address to, uint amount) public returns(bool) {
+    require(balanceOf(msg.sender) >= amount, "balance too low" );
+
+    balances[to] += amount;
+    balances[msg.sender] -= amount;
+
+    emit Transfer(msg.sender, to, amount);
+
+    return true;
+  }
+  // NOTE - Tokens which don’t implement this method
+  // will never flow across the Binance Chain and Binance Smart Chain.
+  // function getOwner() external view returns (address);
 }
--- test/zolve_coin_test.js
+++ test/zolve_coin_test.js
@@ -1,24 +1,60 @@
 const ZolveCoin = artifacts.require("ZolveCoin");
 
 contract("ZolveCoin", function (accounts) {
+  var contract;
+
+  beforeEach(function() {
+     return ZolveCoin.new()
+     .then(function(instance) {
+        contract = instance;
+     });
+  });
+
   it('credits the owners balance with 1', async () => {
-    const contract = await ZolveCoin.deployed();
-    const balance = await contract.getBalanceOfAddress.call(accounts[0]);
+    const balance = await contract.balanceOf(accounts[0]);
 
     assert.equal(balance.valueOf().toString(), '1');
   });
 
   it('has a max cap of 21 million', async () => {
-    const contract = await ZolveCoin.deployed();
-
-    assert.equal((await contract.totalSupply()).toString(), '210000000');
+    assert.equal((await contract.totalSupply()).toString(), '2100000000000000');
     assert.equal((await contract.decimals()).toString(), '8');
   });
 
   it('has a name and symbol', async () => {
-    const contract = await ZolveCoin.deployed();
-
     assert.equal((await contract.name()).toString(), 'Zolve Coin');
     assert.equal((await contract.symbol()).toString(), 'ZLV');
+  });
+
+  it('transfers coins between addresses', async () => {
+    const accountOne = accounts[0];
+    const accountTwo = accounts[1];
+
+    assert.equal((await contract.balanceOf(accountOne)).toString(), '1');
+    assert.equal((await contract.balanceOf(accountTwo)).toString(), '0');
+
+    const amount = 1;
+    await contract.transfer(accountTwo, amount, { from: accountOne });
+
+    assert.equal((await contract.balanceOf(accountOne)).toString(), '0');
+    assert.equal((await contract.balanceOf(accountTwo)).toString(), '1');
+  });
+
+  it('does not tranfer coins if sender has insufficient balance', async () => {
+    const accountOne = accounts[0];
+    const accountTwo = accounts[1];
+
+    assert.equal((await contract.balanceOf(accountOne)).toString(), '1');
+    assert.equal((await contract.balanceOf(accountTwo)).toString(), '0');
+
+    const amount = 1;
+
+    try {
+      await contract.transfer(accountOne, amount, { from: accountTwo })
+      throw "Oops the contract should not have transfered"
+    } catch (e) {
+      assert.equal((await contract.balanceOf(accountOne)).toString(), '1');
+      assert.equal((await contract.balanceOf(accountTwo)).toString(), '0');
+    }
   });
 });
