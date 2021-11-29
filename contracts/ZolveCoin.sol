190ef340c69c3fec3df0ca78994211a00dbc50b8
dobiedad <leomdiv@gmail.com>
2021-11-29 20:45:19 +0000

    add tests for static values

--- contracts/ZolveCoin.sol
+++ contracts/ZolveCoin.sol
@@ -2,13 +2,18 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract ZolveCoin {
+  uint public totalSupply = 210000000;
+  uint public decimals = 8;
+  string public name = "Zolve Coin";
+  string public symbol = "ZLV";
+
   mapping (address => uint) balances;
 
-	constructor() public {
+	constructor() {
 		balances[tx.origin] = 1;
 	}
 
-  function getBalance(address addr) public view returns(uint) {
+  function getBalanceOfAddress(address addr) public view returns(uint) {
 		return balances[addr];
 	}
 }
--- test/zolve_coin_test.js
+++ test/zolve_coin_test.js
@@ -1,10 +1,24 @@
 const ZolveCoin = artifacts.require("ZolveCoin");
 
 contract("ZolveCoin", function (accounts) {
-  it('owner starts with 1 coin', async () => {
+  it('credits the owners balance with 1', async () => {
     const contract = await ZolveCoin.deployed();
-    const balance = await contract.getBalance.call(accounts[0]);
+    const balance = await contract.getBalanceOfAddress.call(accounts[0]);
 
-    assert.equal(balance.valueOf(), 1);
+    assert.equal(balance.valueOf().toString(), '1');
+  });
+
+  it('has a max cap of 21 million', async () => {
+    const contract = await ZolveCoin.deployed();
+
+    assert.equal((await contract.totalSupply()).toString(), '210000000');
+    assert.equal((await contract.decimals()).toString(), '8');
+  });
+
+  it('has a name and symbol', async () => {
+    const contract = await ZolveCoin.deployed();
+
+    assert.equal((await contract.name()).toString(), 'Zolve Coin');
+    assert.equal((await contract.symbol()).toString(), 'ZLV');
   });
 });
